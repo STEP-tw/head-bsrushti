aa8b3b8b46234ea169e7451678567530b5e65fcb
bsrushti <bsrushti@in-bsrushti.local>
2018-12-04 23:42:34 +0530

    Introduces main file
    program working for the following input
    
    node head.js -n 3 file1

--- head.js
+++ head.js
@@ -1,3 +1,30 @@
+const {
+  classifyDetails, 
+  extractNLines,
+  extractNCharacters,
+  readFile
+} = require('./src/lib.js'); 
+
+const { readFileSync } = require('fs'); 
+
+const main = function() { 
+  let details = process.argv;
+  let {option, length, files} = classifyDetails(details.slice(2));
+  let result;
+
+  if(option == '-n') {
+    let contents = readFile(readFileSync, files).join("\n");
+    console.log(extractNLines(length, contents));
+  };
+
+  if(option == '-c') {
+    let contents = readFile(readFileSync, files).join("\n");
+    console.log(extractNCharacters(length, contents));
+  };
+
+};
+main();
+
 /* 
   Usage:
   node ./head.js file1
