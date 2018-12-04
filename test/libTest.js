21ed1878bbd6684cc5e4e07cf6b2a41af42c5a98
bsrushti <bsrushti@in-bsrushti.local>
2018-12-04 22:29:24 +0530

    Introduces classifyDetails function
    classifyDetails function classify the input to
    head [option, length, files]

---
+++ test/libTest.js
@@ -0,0 +1,18 @@
+const {equal, deepEqual} = require('assert');
+const { classifyDetails } = require('../src/lib.js'); 
+
+describe('classifyDetails categories the input according to characteristics', () => {
+  it('should return empty object for no input', () => {
+    deepEqual(classifyDetails(),{});
+  });
+
+  it('should return object of assigned details of file 1', () => {
+    let expectedOutput = { option : '-n', length : 1, files : ['file1'] }
+    deepEqual(classifyDetails(['-n','1','file1']),expectedOutput);
+  });
+
+  it('should return object of assigned details with more than one file', () => {
+    let expectedOutput = { option : '-n', length : 1, files : ['file1','file2','file3'] }
+    deepEqual(classifyDetails(['-n','1','file1','file2','file3']),expectedOutput);
+  });
+});
