7a94a57c20e0f1b79db1f9719035578880814656
Srushti <bsrushti@in-bsrushti.local>
2018-12-10 11:55:07 +0530

    Remove unwanted line from main: extracting option from params

--- head.js
+++ head.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 
 const main = function() { 
   let params = process.argv;
-  let option = classifyDetails(params.slice(2)).option;
   console.log(head(params.slice(2),fs).join("\n")); 
 };
 
