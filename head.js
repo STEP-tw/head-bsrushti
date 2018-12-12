30c3e004ae715a5275e282657ff854d1cdf0a311
Srushti <bsrushti@in-bsrushti.local>
2018-12-12 10:25:14 +0530

    Requiring function getFileData, it was head previously

--- head.js
+++ head.js
@@ -1,12 +1,12 @@
 const {
-  head
+  getFileData
 } = require('./src/lib.js'); 
 
 const fs = require('fs'); 
 
 const main = function() { 
   let params = process.argv;
-  console.log(head(params.slice(1),fs).join("\n")); 
+  console.log(getFileData(params.slice(1),fs).join("\n")); 
 };
 
 main();
