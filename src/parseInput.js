00e33aad102d4e5b058b6b0ab9a674160f6df1da
Srushti <bsrushti@in-bsrushti.local>
2018-12-14 07:04:02 +0530

    Introduces parseInputs function, added test cases

--- src/parseInput.js
+++ src/parseInput.js
@@ -14,9 +14,40 @@ const isOptionWithoutCount = function(option) {
     return isDetailsStartsWithHyphen(option) && option.length == 2;
 };
 
+const parseInputs = function(params) {
+    const defaultOptions = { option: "-n", count: 10, fileNames: params };
+
+    if(isNumberOption(params[0])) {
+        return { 
+        option: "-n",
+        count: params[0].slice(1),
+        fileNames: params.slice(1)
+        };
+    };
+
+    if(isOptionWithoutCount(params[0])) {
+        return {
+            option: params[0].slice(0, 2),
+            count: params[1],
+            fileNames: params.slice(2)
+        };
+    }
+
+    if(isOptionWithCount(params[0])) {
+        return {
+            option: params[0].slice(0, 2),
+            count: params[0].substr(2),
+            fileNames: params.slice(1)
+        };
+    };
+
+    return defaultOptions;
+};
+
 module.exports = { 
     isDetailsStartsWithHyphen,
     isNumberOption,
     isOptionWithCount,
-    isOptionWithoutCount
+    isOptionWithoutCount,
+    parseInputs
  };
--- test/parseInputTest.js
+++ test/parseInputTest.js
@@ -3,7 +3,8 @@ const {
     isDetailsStartsWithHyphen,
     isNumberOption,
     isOptionWithCount,
-    isOptionWithoutCount
+    isOptionWithoutCount,
+    parseInputs
 } = require('../src/parseInput.js');
 
 describe('isDetailsStartsWithHyphen', () => {
@@ -45,5 +46,43 @@ describe('isOptionWithoutCount', function() {
 
     it('should return false if given argument has not a valid option', function() {
         deepEqual(isOptionWithoutCount('-n5'),false);
+    });
+});
+
+describe('parseInputs', function() {
+    it('should classify params when number option is given', function() {
+        let expectedOutput = {
+            option : "-n",
+            count : 3,
+            fileNames : ['words']
+        };
+        deepEqual(parseInputs(['-3', 'words']),expectedOutput);
+    });
+
+    it('should classify params when option is given without count', function() {
+        let expectedOutput = {
+            option : "-n",
+            count : 10,
+            fileNames : ['words']
+        };
+        deepEqual(parseInputs(['words']),expectedOutput);
+    });
+
+    it('should classify params when option is given with count', function() {
+        let expectedOutput = {
+            option : "-n",
+            count : 5,
+            fileNames : ['words']
+        };
+        deepEqual(parseInputs(['-n5','words']),expectedOutput);
+    });
+
+    it('should return default classification no option is provided', function() {
+        let expectedOutput = {
+            option : "-n",
+            count : 10,
+            fileNames : ['words']
+        };
+        deepEqual(parseInputs(['words']),expectedOutput);
     });
 });
