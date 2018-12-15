7cb912f4128daaae7718dfe8c678963ed02cc53e
Srushti <bsrushti@in-bsrushti.local>
2018-12-15 10:41:55 +0530

    Indented parseInput.js

--- src/parseInput.js
+++ src/parseInput.js
@@ -1,53 +1,53 @@
 const isDetailsStartsWithHyphen = function(params) {
-    return params.startsWith("-");
+  return params.startsWith("-");
 };
 
 const isNumberOption = function(option) {
-    return isDetailsStartsWithHyphen(option) && !isNaN(option[1]);
+  return isDetailsStartsWithHyphen(option) && !isNaN(option[1]);
 };
 
 const isOptionWithCount = function(option) {
-    return isDetailsStartsWithHyphen(option) && option.length > 2;
+  return isDetailsStartsWithHyphen(option) && option.length > 2;
 };
 
 const isOptionWithoutCount = function(option) {
-    return isDetailsStartsWithHyphen(option) && option.length == 2;
+  return isDetailsStartsWithHyphen(option) && option.length == 2;
 };
 
 const parseInputs = function(params) {
-    const defaultOptions = { option: "-n", count: 10, fileNames: params };
+  const defaultOptions = { option: "-n", count: 10, fileNames: params };
 
-    if(isNumberOption(params[0])) {
-        return { 
-        option: "-n",
-        count: params[0].slice(1),
-        fileNames: params.slice(1)
-        };
+  if (isNumberOption(params[0])) {
+    return {
+      option: "-n",
+      count: params[0].slice(1),
+      fileNames: params.slice(1)
     };
+  }
 
-    if(isOptionWithoutCount(params[0])) {
-        return {
-            option: params[0].slice(0, 2),
-            count: params[1],
-            fileNames: params.slice(2)
-        };
-    }
-
-    if(isOptionWithCount(params[0])) {
-        return {
-            option: params[0].slice(0, 2),
-            count: params[0].substr(2),
-            fileNames: params.slice(1)
-        };
+  if (isOptionWithoutCount(params[0])) {
+    return {
+      option: params[0].slice(0, 2),
+      count: params[1],
+      fileNames: params.slice(2)
     };
+  }
 
-    return defaultOptions;
+  if (isOptionWithCount(params[0])) {
+    return {
+      option: params[0].slice(0, 2),
+      count: params[0].substr(2),
+      fileNames: params.slice(1)
+    };
+  }
+
+  return defaultOptions;
 };
 
-module.exports = { 
-    isDetailsStartsWithHyphen,
-    isNumberOption,
-    isOptionWithCount,
-    isOptionWithoutCount,
-    parseInputs
- };
+module.exports = {
+  isDetailsStartsWithHyphen,
+  isNumberOption,
+  isOptionWithCount,
+  isOptionWithoutCount,
+  parseInputs
+};
