018a607bea384772c81ee65f3602bba60889fca6
Srushti <bsrushti@in-bsrushti.local>
2018-12-15 10:40:00 +0530

    Indented errorLib.js file

--- src/errorLib.js
+++ src/errorLib.js
@@ -1,31 +1,31 @@
-const countErrorForHead = { 
-    '-n' : "head: illegal line count -- ",
-    '-c' : "head: illegal byte count -- "
-  };
-  
-  const countErrorForTail = { 
-    '-n' : "tail: illegal offset -- ",
-    '-c' : "tail: illegal offset -- "
-  };
-  
-  const invalidCountError = function(option, count, command) {
-    const countError = {
-      head : countErrorForHead,
-      tail : countErrorForTail
-    };
-    return countError[command][option] + count;
-  };
+const countErrorForHead = {
+  "-n": "head: illegal line count -- ",
+  "-c": "head: illegal byte count -- "
+};
 
-  const fileNotFoundError = function(file, command) {
-    return command+": " + file + ": No such file or directory";
-  };
+const countErrorForTail = {
+  "-n": "tail: illegal offset -- ",
+  "-c": "tail: illegal offset -- "
+};
 
-  const getInvalidCountError = function(option, count, command) {
-    return invalidCountError(option, count, command);
+const invalidCountError = function(option, count, command) {
+  const countError = {
+    head: countErrorForHead,
+    tail: countErrorForTail
   };
-  
-  module.exports = {
-      invalidCountError,
-      fileNotFoundError,
-      getInvalidCountError
-  }
+  return countError[command][option] + count;
+};
+
+const fileNotFoundError = function(file, command) {
+  return command + ": " + file + ": No such file or directory";
+};
+
+const getInvalidCountError = function(option, count, command) {
+  return invalidCountError(option, count, command);
+};
+
+module.exports = {
+  invalidCountError,
+  fileNotFoundError,
+  getInvalidCountError
+};
