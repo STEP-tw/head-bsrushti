0544fc8e24e8429bf363c32073b3198fa40f57d5
Srushti <bsrushti@in-bsrushti.local>
2018-12-15 10:42:46 +0530

    Indented tail.js

--- tail.js
+++ tail.js
@@ -1,9 +1,9 @@
-const { getFileData } = require('./src/lib.js'); 
-const fs = require('fs'); 
+const { getFileData } = require("./src/lib.js");
+const fs = require("fs");
 
-const main = function() { 
+const main = function() {
   let params = process.argv;
-  console.log(getFileData(params.slice(2),fs,'tail').join("\n")); 
+  console.log(getFileData(params.slice(2), fs, "tail").join("\n"));
 };
 
 main();
