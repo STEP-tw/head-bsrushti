3076238af656aea95c20fda795968d26459ddd9f
Srushti <bsrushti@in-bsrushti.local>
2018-12-17 17:09:43 +0530

    Introduces extractLines function, added test cases

--- src/util.js
+++ src/util.js
@@ -1,10 +1,19 @@
 const extractContents = function(contents, delimiter, initial, last) {
-    return contents
-      .split(delimiter)
-      .slice(initial, last)
-      .join(delimiter);
-  };
+  return contents
+    .split(delimiter)
+    .slice(initial, last)
+    .join(delimiter);
+};
 
-  module.exports = {
-      extractContents
-  }
+const extractLines = function(command, count, contents) {
+  let linesAsCommand = {
+    head: extractContents(contents, "\n", 0, count),
+    tail: extractContents(contents, "\n", -count, contents.split("\n").length)
+  };
+  return linesAsCommand[command];
+};
+
+module.exports = {
+  extractContents,
+  extractLines
+};
--- test/utilTest.js
+++ test/utilTest.js
@@ -1,17 +1,66 @@
-const {extractContents } = require('../src/util.js');
-const assert = require('assert');
+const assert = require("assert");
+const { extractContents, extractLines } = require("../src/util.js");
 
 describe("extractContents returns contents as per the delimiter it has passed", () => {
-    it("should return contents separated by \n for head", () => {
-     let actual = extractContents("first line\nsecond line", "\n", 0, 2);
-     let expected = "first line\nsecond line";
+  it("should return contents separated by \n for head", () => {
+    let actual = extractContents("first line\nsecond line", "\n", 0, 2);
+    let expected = "first line\nsecond line";
+    assert.deepEqual(actual, expected);
+  });
+
+  it("should return contents separated by character for head", () => {
+    let actual = extractContents("first line\nsecond line", "", 0, 5);
+    let expected = "first";
+    assert.deepEqual(actual, expected);
+  });
+});
+
+describe("extractLines", function() {
+  describe("For head", () => {
+    it("should return lines empty string for 0 length given", function() {
+      let actual = extractLines("head", 0, "first\nline\nsecond\nline");
+      let expected = "";
       assert.deepEqual(actual, expected);
     });
-  
-    it("should return contents separated by character for head", () => {
-      let actual = extractContents("first line\nsecond line", "", 0, 5);
-      let expected = "first";
+
+    it("should return one character for length as input 1", () => {
+      let actual = extractLines("head", 1, "first line\nsecond line");
+      let expected = "first line";
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return number of lines as per the given length", () => {
+      let actual = extractLines("head", 2, "first line\nsecond line");
+      let expected = "first line\nsecond line";
+      assert.deepEqual(actual, expected);
+
+      actual = extractLines("head", 4, "first\nline\nsecond\nline");
+      expected = "first\nline\nsecond\nline";
       assert.deepEqual(actual, expected);
     });
   });
-  
+
+  describe("For tail", function() {
+    it("should return whole string for 0 length input", () => {
+      let actual = extractLines("tail", 0, "first line\nsecond line");
+      let expected = "first line\nsecond line";
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return one character for length as input 1", () => {
+      let actual = extractLines("tail", 1, "first line\nsecond line");
+      let expected = "second line";
+      assert.deepEqual(actual, expected);
+    });
+
+    it("should return number of characters as per the given length", () => {
+      let actual = extractLines('tail', 2, "first line\nsecond line");
+      let expected = "first line\nsecond line";
+      assert.deepEqual(actual, expected);
+
+      actual = extractLines('tail', 5, "first\nline\nsecond\nline");
+      expected = "first\nline\nsecond\nline";
+      assert.deepEqual(actual, expected);
+    });
+  });
+});
