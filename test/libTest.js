5dafe461fc9ddfb5258e8137bf401df857867d1f
Srushti <bsrushti@in-bsrushti.local>
2018-12-17 09:27:41 +0530

    Rename expectedOutput to expected in libTest.js

--- test/libTest.js
+++ test/libTest.js
@@ -151,8 +151,8 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-c3", "file3"], fs, "head");
-    let expectedOutput = ["fir"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["fir"];
+    deepEqual(actual, expected);
   });
 
   it("should return the file data if -n input is given with length and file", () => {
@@ -163,8 +163,8 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-n2", "file1"], fs, "head");
-    let expectedOutput = ['A\nB'];
-    deepEqual(actual, expectedOutput);
+    let expected = ['A\nB'];
+    deepEqual(actual, expected);
   });
 
   it("should return the file content in default case of length 10", () => {
@@ -175,8 +175,8 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["file1"], fs, "head");
-    let expectedOutput = ['A\nB\nC\nD\nE\nF\nG\nH\nI\nJ'];
-    deepEqual(actual, expectedOutput);
+    let expected = ['A\nB\nC\nD\nE\nF\nG\nH\nI\nJ'];
+    deepEqual(actual, expected);
   });
 
   it("should return error message if type is -n and length is not provided", () => {
@@ -187,8 +187,8 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-n", "file2"], fs, "head");
-    let expectedOutput = ["head: illegal line count -- file2"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["head: illegal line count -- file2"];
+    deepEqual(actual, expected);
   });
 
   it("should return file content with header if more than two files are provided", () => {
@@ -199,15 +199,15 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-c3", "file1", "file2"], fs, "head");
-    let expectedOutput = ["==> file1 <==\nA\nB", "==> file2 <==\na\nb"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["==> file1 <==\nA\nB", "==> file2 <==\na\nb"];
+    deepEqual(actual, expected);
 
     actual = getFileData(["-n3", "file1", "file2"], fs, "head");
-    expectedOutput = [
+    expected = [
       "==> file1 <==\nA\nB\nC",
       "==> file2 <==\na\nb\nc"
     ];
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
   });
 
   it("should return error message if file doesn't exist for single file", () => {
@@ -217,8 +217,8 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-c3", "file1"], fs, "head");
-    let expectedOutput = ["head: file1: No such file or directory"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["head: file1: No such file or directory"];
+    deepEqual(actual, expected);
   });
 
   it("should return error message if file doesn't exist if more than one file provided", () => {
@@ -228,9 +228,9 @@ describe("getFileData for head", () => {
     };
 
     let actual = getFileData(["-c3", "file1","file2"], fs, "head");
-    let expectedOutput = ["head: file1: No such file or directory",
+    let expected = ["head: file1: No such file or directory",
                           "head: file2: No such file or directory"];
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
   });
 });
 
@@ -250,8 +250,8 @@ describe("getFileData for tail", () => {
     };
     
     let actual = getFileData(["-c0", "file1"], fs, "tail");
-    let expectedOutput = [];
-    deepEqual(actual, expectedOutput);
+    let expected = [];
+    deepEqual(actual, expected);
   });
 
   it("should return empty array if 0 as a count is provided with option -n", () => {
@@ -262,8 +262,8 @@ describe("getFileData for tail", () => {
     };
     
     let actual = getFileData(["-n0", "file1"], fs, "tail");
-    let expectedOutput = [];
-    deepEqual(actual, expectedOutput);
+    let expected = [];
+    deepEqual(actual, expected);
   });
 
   it("should return the file data if -n input is given with length and files", () => {
@@ -274,8 +274,8 @@ describe("getFileData for tail", () => {
     };
 
     let actual = getFileData(["-c3", "file3"], fs, "tail");
-    let expectedOutput = ["ine"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["ine"];
+    deepEqual(actual, expected);
   });
 
   it("should return the file data if -n input is given with length and files", () => {
@@ -286,8 +286,8 @@ describe("getFileData for tail", () => {
     };
 
     let actual = getFileData(["-n2", "file1"], fs, "tail");
-    let expectedOutput = ["O\nP"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["O\nP"];
+    deepEqual(actual, expected);
   });
 
   it("should return the file content in default case of length 10", () => {
@@ -298,8 +298,8 @@ describe("getFileData for tail", () => {
     };
     
     let actual = getFileData(["file1"], fs, "tail");
-    let expectedOutput = ["G\nH\nI\nJ\nK\nL\nM\nN\nO\nP"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["G\nH\nI\nJ\nK\nL\nM\nN\nO\nP"];
+    deepEqual(actual, expected);
   });
 
   it("should return error message if type is -n and length is not provided", () => {
@@ -310,8 +310,8 @@ describe("getFileData for tail", () => {
     };
     
     let actual = getFileData(["-n", "file1"], fs, "tail");
-    let expectedOutput = ["tail: illegal offset -- file1"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["tail: illegal offset -- file1"];
+    deepEqual(actual, expected);
   });
 
   it("should return file content with header if more than two files are provided", () => {
@@ -322,15 +322,15 @@ describe("getFileData for tail", () => {
     };
 
     let actual = getFileData(["-c3", "file2", "file3"], fs, "tail");
-    let expectedOutput = ["==> file2 <==\no\np", "==> file3 <==\nine"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["==> file2 <==\no\np", "==> file3 <==\nine"];
+    deepEqual(actual, expected);
 
     actual = getFileData(["-n3", "file1", "file2"], fs, "tail");
-    expectedOutput = [
+    expected = [
       "==> file1 <==\nN\nO\nP",
       "==> file2 <==\nn\no\np"
     ];
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
   });
 
   it("should return error message if file doesn't exist for single file", () => {
@@ -340,8 +340,8 @@ describe("getFileData for tail", () => {
     };
 
     let actual = getFileData(["-c3", "file1"], fs, "tail");
-    let expectedOutput = ["tail: file1: No such file or directory"];
-    deepEqual(actual, expectedOutput);
+    let expected = ["tail: file1: No such file or directory"];
+    deepEqual(actual, expected);
   });
 
   it("should return error message if file doesn't exist if more than one file provided", () => {
@@ -351,9 +351,9 @@ describe("getFileData for tail", () => {
     };
 
     let actual = getFileData(["-c3", "file1","file2"], fs, "tail");
-    let expectedOutput = ["tail: file1: No such file or directory",
+    let expected = ["tail: file1: No such file or directory",
                           "tail: file2: No such file or directory"];
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
   });
 });
 
@@ -527,13 +527,13 @@ describe("format", function() {
   it("should add header and return content of given file", function() {
     let fileName = "words";
     let actual = format(fs.readline, extractHeadCharacters, fileName, 3);
-    let expectedOutput = "==> words <==\none";
+    let expected = "==> words <==\none";
     
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
 
     fileName = "numbers";
-    expectedOutput = "==> numbers <==\ntwo\nthree";
+    expected = "==> numbers <==\ntwo\nthree";
     actual = format(fs.readline, extractTailLines, fileName, 2);
-    deepEqual(actual, expectedOutput);
+    deepEqual(actual, expected);
   });
 });
