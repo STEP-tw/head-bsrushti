c3ff7d09a06a446e88e3607b3eb222c0062ea730
Srushti <bsrushti@in-bsrushti.local>
2018-12-17 09:47:26 +0530

    Extract input and output to actual and expected in errorLibTest.js

--- test/errorLibTest.js
+++ test/errorLibTest.js
@@ -7,62 +7,62 @@ const {
 
 describe("invalidCountError", () => {
   it("should return error message if invalid length provided with option -c and command head", () => {
-    equal(
-      invalidCountError("-c", "1q", "head"),
-      "head: illegal byte count -- 1q"
-    );
-    equal(
-      invalidCountError("-c", "aaa", "head"),
-      "head: illegal byte count -- aaa"
-    );
+    let actual = invalidCountError("-c", "1q", "head");
+    let expected = "head: illegal byte count -- 1q";
+    equal(actual, expected);
+
+    actual = invalidCountError("-c", "aaa", "head");
+    expected = "head: illegal byte count -- aaa";
+    equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -n and command head", () => {
-    equal(
-      invalidCountError("-n", "1q", "head"),
-      "head: illegal line count -- 1q"
-    );
-    equal(
-      invalidCountError("-n", "aaa", "head"),
-      "head: illegal line count -- aaa"
-    );
+    let actual = invalidCountError("-n", "1q", "head");
+    let expected = "head: illegal line count -- 1q";
+    equal(actual, expected);
+
+    actual = invalidCountError("-n", "aaa", "head");
+    expected = "head: illegal line count -- aaa";
+    equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -c and command tail", () => {
-    equal(invalidCountError("-c", "1q", "tail"), "tail: illegal offset -- 1q");
-    equal(
-      invalidCountError("-c", "aaa", "tail"),
-      "tail: illegal offset -- aaa"
-    );
+    let actual = invalidCountError("-c", "1q", "tail");
+    let expected = "tail: illegal offset -- 1q";
+    equal(actual, expected);
+
+    actual = invalidCountError("-c", "aaa", "tail");
+    expected = "tail: illegal offset -- aaa";
+    equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -n and command tail", () => {
-    equal(invalidCountError("-n", "1q", "tail"), "tail: illegal offset -- 1q");
-    equal(
-      invalidCountError("-n", "aaa", "tail"),
-      "tail: illegal offset -- aaa"
-    );
+    let actual = invalidCountError("-n", "1q", "tail");
+    let expected = "tail: illegal offset -- 1q";
+    equal(actual, expected);
+
+    actual = invalidCountError("-n", "aaa", "tail");
+    expected = "tail: illegal offset -- aaa";
+    equal(actual, expected);
   });
 });
 
 describe("fileNotFoundError", () => {
   it("should return error message if it not finds the file", () => {
-    equal(
-      fileNotFoundError("file", "head"),
-      "head: file: No such file or directory"
-    );
+    let actual = fileNotFoundError("file", "head");
+    let expected = "head: file: No such file or directory";
+    equal(actual, expected);
   });
 });
 
 describe("getInvalidCountError", function() {
   it("should return error message according to given params", function() {
-    equal(
-      getInvalidCountError("-n", "1q", "tail"),
-      "tail: illegal offset -- 1q"
-    );
-    equal(
-      getInvalidCountError("-n", "aaa", "head"),
-      "head: illegal line count -- aaa"
-    );
+    let actual = getInvalidCountError("-n", "1q", "tail");
+    let expected = "tail: illegal offset -- 1q";
+    equal(actual, expected);
+
+    actual = getInvalidCountError("-n", "aaa", "head");
+    expected = "head: illegal line count -- aaa";
+    equal(actual, expected);
   });
 });
