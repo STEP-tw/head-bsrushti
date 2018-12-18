07b198ad5aa9e58fdd78d9c2e6ff94fba7a17ab0
Srushti <bsrushti@in-bsrushti.local>
2018-12-18 10:27:33 +0530

    Remove destructuring of equal in errorLibTest
    * Using equal explicitely

--- test/errorLibTest.js
+++ test/errorLibTest.js
@@ -1,4 +1,4 @@
-const { equal } = require("assert");
+const assert = require("assert");
 const {
   getInvalidCountError,
   fileNotFoundError,
@@ -8,41 +8,41 @@ describe("getInvalidCountError", () => {
   it("should return error message if invalid length provided with option -c and command head", () => {
     let actual = getInvalidCountError("-c", "1q", "head");
     let expected = "head: illegal byte count -- 1q";
-    equal(actual, expected);
+    assert.equal(actual, expected);
 
     actual = getInvalidCountError("-c", "aaa", "head");
     expected = "head: illegal byte count -- aaa";
-    equal(actual, expected);
+    assert.equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -n and command head", () => {
     let actual = getInvalidCountError("-n", "1q", "head");
     let expected = "head: illegal line count -- 1q";
-    equal(actual, expected);
+    assert.equal(actual, expected);
 
     actual = getInvalidCountError("-n", "aaa", "head");
     expected = "head: illegal line count -- aaa";
-    equal(actual, expected);
+    assert.equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -c and command tail", () => {
     let actual = getInvalidCountError("-c", "1q", "tail");
     let expected = "tail: illegal offset -- 1q";
-    equal(actual, expected);
+    assert.equal(actual, expected);
 
     actual = getInvalidCountError("-c", "aaa", "tail");
     expected = "tail: illegal offset -- aaa";
-    equal(actual, expected);
+    assert.equal(actual, expected);
   });
 
   it("should return error message if invalid length provided with option -n and command tail", () => {
     let actual = getInvalidCountError("-n", "1q", "tail");
     let expected = "tail: illegal offset -- 1q";
-    equal(actual, expected);
+    assert.equal(actual, expected);
 
     actual = getInvalidCountError("-n", "aaa", "tail");
     expected = "tail: illegal offset -- aaa";
-    equal(actual, expected);
+    assert.equal(actual, expected);
   });
 });
 
@@ -50,6 +50,6 @@ describe("fileNotFoundError", () => {
   it("should return error message if it not finds the file", () => {
     let actual = fileNotFoundError("file", "head");
     let expected = "head: file: No such file or directory";
-    equal(actual, expected);
+    assert.equal(actual, expected);
   });
 });
